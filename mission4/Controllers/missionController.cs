0c6a2307dd69715a9f11f302afd465ff3fcb92fc
tannergwood <tg357@byu.edu>
2023-02-06 14:49:33 -0700

    Conversion to MVC

---
+++ mission4/Controllers/missionController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mission4.Controllers
+{
+    public class MissionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Mission()
+        {
+            return View();
+        }
+    }
+}
--- mission4/Startup.cs
+++ mission4/Startup.cs
@@ -21,8 +21,22 @@ namespace mission4
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseDefaultFiles();
+            if (env.IsEnvironment("Development"))
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
             app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=mission}/{action=Index}/{id?}"
+                    );
+            });
         }
     }
 }
